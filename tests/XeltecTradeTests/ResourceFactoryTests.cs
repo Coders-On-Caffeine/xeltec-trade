70f74d96794284c17f0769dcb5a6ba49f5082b8a
trwest <trwest@gmail.com>
2016-03-22 10:26:10 +0000

    created ResourceFactoryTests

---
+++ tests/XeltecTradeTests/ResourceFactoryTests.cs
@@ -0,0 +1,25 @@
+﻿
+namespace XeltecTradeTests
+{
+    using Xeltec.Trade;
+
+    using Moq.AutoMock;
+    using Xunit;
+
+    public class ResourceFactoryTests
+    {
+        private AutoMocker autoMocker;
+
+        public ResourceFactoryTests()
+        {
+            autoMocker = new AutoMocker();
+        }
+
+        [Fact]
+        public void CanConstructResourceFactory()
+        {
+            var sut = autoMocker.CreateInstance<ResourceFactory>();
+            Assert.NotNull(sut);
+        }
+    }
+}
