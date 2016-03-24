04bff0faa6875df519afbb466b95495af453a73a
trwest <trwest@gmail.com>
2016-03-24 11:32:05 +0000

    TradableStock updates
    Can create TradableStock
    AddStock adds provided quantity
    AddStock throws ArgumentOutOfRangeException when provided quantity is zero
    AddStock throws ArgumentOutOfRangeException when provided quantity is less than zero
    RemoveStock removes provided quantity
    RemoveStock throws ArgumentOutOfRangeException when provided quantity is zero
    RemoveStock throws ArgumentOurOfRangeException when provided quantity is less than zero
    RemoveStock returns false when QuantityInStock is less than quantity removed

--- src/Xeltec.Trade/TradableStock.cs
+++ src/Xeltec.Trade/TradableStock.cs
@@ -1,6 +1,8 @@
 ﻿
 namespace Xeltec.Trade
 {
+    using System;
+
     using Xeltec.Trade.Interfaces;
     using Xeltec.Trade.Interfaces.TradeResources;
 
@@ -24,6 +26,34 @@ namespace Xeltec.Trade
         public T TradeItem
         {
             get; private set;
+        }
+
+        public bool AddStock(double quantity)
+        {
+            if(quantity <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(quantity));
+            }
+            QuantityInStock += quantity;
+
+            return true;
+        }
+
+        public bool RemoveStock(double quantity)
+        {
+            if (quantity <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(quantity));
+            }
+
+            if(quantity > QuantityInStock)
+            {
+                return false;
+            }
+
+            QuantityInStock -= quantity;
+
+            return true;
         }
     }
 }
